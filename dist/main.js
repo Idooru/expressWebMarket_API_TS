8ab160c8aee8afe7995299565ae3c1a6c976ab10
Idooru <shere1765@gmail.com>
2022-04-24 13:19:07 +0900

    0.4.2.4

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const str = "My first TypeScript!";
+console.log(str);
+console.log(1);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const str: string = "My first TypeScript!";
+
+console.log(str);
+console.log(1);
